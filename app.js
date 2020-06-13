2748b997b96046f959b7f1f1b2e6dafdf9edb293
pravalika-cpu <65751881+pravalika-cpu@users.noreply.github.com>
2020-06-13 13:52:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,8 +3,13 @@ let minutes=0;
 let hours=0;
 function stopwatch(){
     seconds++
-    if(seconds==60){
+    if(seconds/60==1){
+        seconds=0;
         minutes++
+        if(minutes/60==1){
+            minutes=0;
+            hours++
+        }
     }
 
 
